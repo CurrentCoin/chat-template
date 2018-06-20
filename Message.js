6490239205d79fff87a6aa74a630a5af1855bb5b
jason00111 <emberleyjason@gmail.com>
2018-06-19 21:31:58 -0700

    moment works

--- Message.js
+++ Message.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Moment from 'react-moment';
-import 'moment-timezone';
 
 class Message extends React.Component {
   render() {
     // Was the message sent by the current user. If so, add a css class
     const fromMe = this.props.fromMe ? 'from-me' : '';
-    // const rightNow = Date.now();
-    // const currentTime = this.props.rightNow;
+
     // if the date is today, show "Today" "5:00 pm"
     // else if the date was sent yesterday, show "June 12, 2018" "5:00 pm"
     return (
@@ -17,9 +15,12 @@ class Message extends React.Component {
             { this.props.username }
           </div>
           <div className='message-datetime'>
-            {/* <Moment> */}
+            <Moment
+              fromNow
+              interval={30000}
+            >
               { this.props.currentTime }
-            {/* </Moment> */}
+            </Moment>
           </div>
         </div>
         <div className='message-body'>
